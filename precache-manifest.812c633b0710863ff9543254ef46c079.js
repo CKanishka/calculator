1d4ef98ca706e5136f7354c8be3224272c47a3c5
CKanishka <ckanishka24@gmail.com>
2020-01-13 02:05:09 +0530

    Updates

---
+++ precache-manifest.812c633b0710863ff9543254ef46c079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4226246e0efb6df16a04738d08d36459",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "b1fabb6835653c3771e0",
+    "url": "/calculator/static/css/main.f7861909.chunk.css"
+  },
+  {
+    "revision": "04b8a792aada1b57e55d",
+    "url": "/calculator/static/js/2.4ee8b5ba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator/static/js/2.4ee8b5ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1fabb6835653c3771e0",
+    "url": "/calculator/static/js/main.04347640.chunk.js"
+  },
+  {
+    "revision": "6dd5921c6be255dbaa89",
+    "url": "/calculator/static/js/runtime-main.954e8902.js"
+  }
+]);
